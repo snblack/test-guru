c14865dec2221c7981ef61a5cbc8048e698911eb
Snblack <koracell@yandex.ru>
2020-06-01 00:13:34 +0300

    v11

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   def tests_with_level(level)
-    @level = level
     Test.joins('JOIN reports ON reports.test_id = tests.id').
          joins('JOIN users ON users.id = reports.user_id').
-        where("users.id = #{self.id} AND tests.level = :level", level: @level)
+        where("users.id = :user AND tests.level = :level", level: level, user: self.id)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,54 +5,58 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Report.delete_all
+User.delete_all
+Answer.delete_all
+Question.delete_all
+Test.delete_all
+Category.delete_all
+
+cat1 = Category.create(title: 'Программирование')
+cat2 = Category.create(title: 'Биология')
+cat3 = Category.create(title: 'Физика')
+cat4 = Category.create(title: 'История')
+cat5 = Category.create(title: 'Статистика')
+
+test1 = Test.create(title: 'Ruby Start', level: 1, category_id: cat1.id)
+test2 = Test.create(title: 'Ruby Basic', level: 2, category_id: cat2.id)
+test3 = Test.create(title: 'Ruby Advanse', level: 3, category_id: cat3.id)
+test4 = Test.create(title: 'Великая отечественная война', level: 2, category_id: cat4.id)
+test5 = Test.create(title: 'Черные дыры', level: 5, category_id: cat5.id)
+
+question1 = Question.create(body: 'Как метод выводить текст в консоль?', test_id: test1.id)
+question2 = Question.create(body: 'Как создается хэш?', test_id: test2.id)
+question3 = Question.create(body: 'Какой код подгружает посевные дынне в базу данных?', test_id: test3.id)
+question4 = Question.create(body: 'В каком году началась Великая отечественная война?', test_id: test4.id)
+question5 = Question.create(body: 'В каком году впервые была сфотографирована сверхмассивная
+чёрная дыра в центре галактики Messier 87?', test_id: test5.id)
+
+Answer.create(body: 'loop', correct: false, question_id: question1.id)
+Answer.create(body: 'zoom', correct: false, question_id: question1.id)
+Answer.create(body: 'puts', correct: true, question_id: question1.id)
+
+Answer.create(body: 'Hash.new', correct: true, question_id: question2.id)
+Answer.create(body: 'h = []', correct: false, question_id: question2.id)
+Answer.create(body: 'load', correct: false, question_id: question2.id)
+
+Answer.create(body: 'nails log -gpaph', correct: false, question_id: question3.id)
+Answer.create(body: 'rails db:seed', correct: true, question_id: question3.id)
+Answer.create(body: 'git commit', correct: false, question_id: question3.id)
+
+Answer.create(body: '1914', correct: false, question_id: question4.id)
+Answer.create(body: '1941', correct: true, question_id: question4.id)
+Answer.create(body: '1915', correct: false, question_id: question4.id)
+
+Answer.create(body: '2019', correct: true, question_id: question5.id)
+Answer.create(body: '1515', correct: false, question_id: question5.id)
+Answer.create(body: '2001', correct: false, question_id: question5.id)
 
 
-# Category.create(title: 'Программирование')
-# Category.create(title: 'Биология')
-# Category.create(title: 'Физика')
-# Category.create(title: 'История')
-# Category.create(title: 'Статистика')
-#
-# Test.create(title: 'Ruby Start', level: 1, category_id: 21)
-# Test.create(title: 'Ruby Basic', level: 2, category_id: 22)
-# Test.create(title: 'Ruby Advanse', level: 3, category_id: 23)
-#
-# Test.create(title: 'Великая отечественная война', level: 2, category_id: 24)
-# Test.create(title: 'Черные дыры', level: 5, category_id: 25)
-#
-# Question.create(body: 'Как метод выводить текст в консоль?', test_id: 16)
-# Question.create(body: 'Как создается хэш?', test_id: 17)
-# Question.create(body: 'Какой код подгружает посевные дынне в базу данных?', test_id: 18)
-# Question.create(body: 'В каком году началась Великая отечественная война?', test_id: 19)
-# Question.create(body: 'В каком году впервые была сфотографирована сверхмассивная
-# чёрная дыра в центре галактики Messier 87?', test_id: 20)
-#
-# Answer.create(body: 'loop', correct: false, question_id:16)
-# Answer.create(body: 'zoom', correct: false, question_id:16)
-# Answer.create(body: 'puts', correct: true, question_id:16)
-#
-# Answer.create(body: 'Hash.new', correct: true, question_id:17)
-# Answer.create(body: 'h = []', correct: false, question_id:17)
-# Answer.create(body: 'load', correct: false, question_id:17)
-#
-# Answer.create(body: 'nails log -gpaph', correct: false, question_id:18)
-# Answer.create(body: 'rails db:seed', correct: true, question_id:18)
-# Answer.create(body: 'git commit', correct: false, question_id:18)
-#
-# Answer.create(body: '1914', correct: false, question_id:19)
-# Answer.create(body: '1941', correct: true, question_id:19)
-# Answer.create(body: '1915', correct: false, question_id:19)
-#
-# Answer.create(body: '2019', correct: true, question_id:20)
-# Answer.create(body: '1515', correct: false, question_id:20)
-# Answer.create(body: '2001', correct: false, question_id:20)
+user1 = User.create(first_name: 'Татьяна', last_name: 'Трубчик')
+user2 = User.create(first_name: 'Александр', last_name: 'Черняк')
 
+Report.create(test_id: test1.id, user_id: user1.id, status: 'Прошел')
+Report.create(test_id: test2.id, user_id: user1.id, status: 'В процессе')
 
-# User.create(first_name: 'Татьяна', last_name: 'Трубчик')
-# User.create(first_name: 'Александр', last_name: 'Черняк')
-
-Report.create(test_id: 16, user_id: 1, status: 'Прошел')
-Report.create(test_id: 17, user_id: 1, status: 'В процессе')
-
-Report.create(test_id: 19, user_id: 2, status: 'Прошел')
-Report.create(test_id: 20, user_id: 2, status: 'В процессе')
+Report.create(test_id: test4.id, user_id: user2.id, status: 'Прошел')
+Report.create(test_id: test5.id, user_id: user2.id, status: 'В процессе')
