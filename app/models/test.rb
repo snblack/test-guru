67c221994b276b0475916d296a5990181bb9cb57
Snblack <koracell@yandex.ru>
2020-06-02 23:24:48 +0300

    v14

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,11 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :author
+  has_many :questions
+  has_many :tests_users
+  has_many :users, through: tests_users
+
+
   def self.test_with_category(category)
     Test.joins('JOIN categories ON categories.id = tests.category_id').
          where("categories.title = :category", category: category).
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_074338) do
+ActiveRecord::Schema.define(version: 2020_06_02_202125) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2020_06_01_074338) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "categories", force: :cascade do |t|
@@ -51,7 +58,15 @@ ActiveRecord::Schema.define(version: 2020_06_01_074338) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "category_id"
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+  end
+
+  create_table "tests_users", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "test_id", null: false
+    t.index ["user_id", "test_id"], name: "index_tests_users_on_user_id_and_test_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
@@ -65,5 +80,6 @@ ActiveRecord::Schema.define(version: 2020_06_01_074338) do
   add_foreign_key "questions", "tests"
   add_foreign_key "reports", "tests"
   add_foreign_key "reports", "users"
+  add_foreign_key "tests", "authors"
   add_foreign_key "tests", "categories"
 end
