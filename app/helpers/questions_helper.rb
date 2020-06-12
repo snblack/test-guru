b4fc7ed00c83d0d28d2d3ff684037312bbb3757a
Snblack <koracell@yandex.ru>
2020-06-12 23:40:45 +0300

    v22

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,19 @@
 module QuestionsHelper
+  def question_header
+    case params['action']
+    when 'new'
+      "Create New #{@test.title} Question"
+    when 'edit'
+      "Edit #{Test.find(@question.test_id).title}"
+    end
+  end
+
+  def current_year
+    Time.now.year
+  end
+
+  def github_url(author, repo)
+    "https://github.com/#{author}/#{repo}"
+  end
+
 end
