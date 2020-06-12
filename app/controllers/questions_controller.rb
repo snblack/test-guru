f8c725503cbea1f6ecdc95f2278f346af6390a7e
Snblack <koracell@yandex.ru>
2020-06-12 23:42:20 +0300

    v23

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,7 +4,7 @@ class QuestionsController < ApplicationController
   before_action :find_test, only: %i[index create]
 
   def index
-    @questions = @test.question
+    @questions = @test.questions
   end
   # /questions/:id(.:format)
   def show
