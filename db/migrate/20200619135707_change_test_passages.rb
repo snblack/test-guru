2e9012b3ee5a78c479c8815f534b92f2a7db3564
Snblack <koracell@yandex.ru>
2020-07-15 11:26:49 +0300

    v51

--- db/migrate/20200619135707_change_test_passages.rb
+++ db/migrate/20200619135707_change_test_passages.rb
@@ -5,11 +5,11 @@ class ChangeTestPassages < ActiveRecord::Migration[6.0]
     create_table :test_passages do |t|
       t.references :user, null: false, foreign_key: true
       t.references :test, null: false, foreign_key: true
-      t.references :current_question, foreign_key: { to_table: :questions }
       t.integer :correct_questions, default:0
       t.integer :num_question
 
       t.timestamps
     end
+    add_foreign_key :current_question, :questions, column: :question_id
   end
 end
