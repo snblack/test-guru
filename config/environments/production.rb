ab3fc9e543379febd363e1d225081c16b9a74126
Snblack <koracell@yandex.ru>
2020-07-15 20:52:01 +0300

    v61

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port:      587,
-    user_name: ENV['SMTP_USERNAME'],
-    password:  ENV['SMTP_PASSWORD'],
+    user_name: 'snblack06@gmail.com',
+    password:  'Koracell06',
     authentication: 'plain',
     enable_starttls_auto: true
   }
