98cd39b5e9f4ecb6299c96542e2fa480b6cc8efb
Snblack <koracell@yandex.ru>
2020-07-15 20:44:19 +0300

    v60

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
     port:      587,
     user_name: ENV['SMTP_USERNAME'],
     password:  ENV['SMTP_PASSWORD'],
-    authentication: :plain,
+    authentication: 'plain',
     enable_starttls_auto: true
   }
 
