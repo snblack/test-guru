576e35f7b479824d1d9e4cc0ee78691f1ae8dbac
Snblack <koracell@yandex.ru>
2020-07-15 20:18:41 +0300

    v59

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,12 +64,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'floating-tundra-49796.herokuapp.com' }
   config.action_mailer.delivery_method = :smpt
   config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :user_name => ENV['SMTP_USERNAME'],
-    :password => ENV['SMTP_PASSWORD'],
-    :authentication => :plain,
-    :enable_starttls_auto => true
+    address: "smtp.gmail.com",
+    port:      587,
+    user_name: ENV['SMTP_USERNAME'],
+    password:  ENV['SMTP_PASSWORD'],
+    authentication: :plain,
+    enable_starttls_auto: true
   }
 
 
