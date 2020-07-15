857524a0c47b1d2197e5b170017e8f0f953f714b
Snblack <koracell@yandex.ru>
2020-07-15 11:58:03 +0300

    v54

--- db/migrate/20200619135707_change_test_passages.rb
+++ db/migrate/20200619135707_change_test_passages.rb
@@ -5,7 +5,7 @@ class ChangeTestPassages < ActiveRecord::Migration[6.0]
     create_table :test_passages do |t|
       t.references :user, null: false, foreign_key: true
       t.references :test, null: false, foreign_key: true
-      t.references :question, null: false, foreign_key: true
+      # t.references :question, null: false, foreign_key: true
       t.integer :correct_questions, default:0
       t.integer :num_question
 
